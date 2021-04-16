ad4f9e1f2d6958d89760cd2b51fa05d36272244d
alfi sahri <alfisahri33@gmail.com>
2021-04-16 10:55:05 +0700

    add access into pegawaiController

--- app/Http/Controllers/Pegawai/PegawaiController.php
+++ app/Http/Controllers/Pegawai/PegawaiController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Pegawai;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Employee;
 
 class PegawaiController extends Controller
 {
     public function index()
     {
-        return view("pegawai.index");
+        $employee = Employee::where("user_id",auth()->user()->id)->first();
+        return view("pegawai.index", ["employee"=>$employee]);
     }
 }
