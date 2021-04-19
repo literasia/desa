f57bc8afe0d346a23b9271b6fc9943c85331dc88
alfi sahri <alfisahri33@gmail.com>
2021-04-20 01:14:57 +0700

    create attendances table

---
+++ database/migrations/2021_04_19_173228_create_attendances_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttendancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('employee_id');
+            $table->unsignedBigInteger('village_id');
+            $table->date('date_attendance');
+            $table->char('status', 1);
+            $table->unsignedBigInteger('editor_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendances');
+    }
+}
