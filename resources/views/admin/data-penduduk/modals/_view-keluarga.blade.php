17ecb4c7db2a521eab93e9c0b04ad068a81c4e37
nisasaf <nisasaff99@gmail.com>
2021-04-22 14:46:45 +0700

    update modal keluarga

--- resources/views/admin/data-penduduk/modals/_view-keluarga.blade.php
+++ resources/views/admin/data-penduduk/modals/_view-keluarga.blade.php
@@ -11,16 +11,64 @@
             </div>
             <div class="modal-body p-4">
                 <div class="alert alert-primary shadow">
-                    <div class="badge badge-info rounded mb-3"><h6 class="m-0">NIK</h6></div>
-                    <h6 class="text-dark font-weight-bold">1234567890345</h6>
+                    <div class="badge badge-info rounded mb-3"><h6 class="m-0">Nomor Kartu Keluarga</h6></div>
+                    <h5 class="text-dark font-weight-bold">1234567890345</h5>
                 </div>
 
                 <div class="mt-5" style="position: relative;">
-                    <h6 class="font-weight-bold d-inline-block mt-4 px-2" style="position: absolute; top: -35px; left: 30px; background: #fff;">Anggota Keluarga</h6>
+                    <h6 class="font-weight-bold d-inline-block mt-4 px-2" style="position: absolute; top: -35px; left: 20px; background: #fff;">Anggota Keluarga</h6>
                     <div class="border rounded p-3">
-                      
+                        <table class="table table-striped">
+                            <tr>
+                                <td>Asdf</td>
+                                <td>12356789689</td>
+                                <td>Kepala Keluarga</td>
+                            </tr>
+                            <tr>
+                                <td>Ghjk</td>
+                                <td>12356789689</td>
+                                <td>Ibu</td>
+                            </tr>
+                            <tr>
+                                <td>Qwerty</td>
+                                <td>12356789689</td>
+                                <td>Anak</td>
+                            </tr>
+                            <tr>
+                                <td>Yuiop</td>
+                                <td>12356789689</td>
+                                <td>Anak</td>
+                            </tr>
+                        </table>
                     </div>
                 </div>
+
+                <div class="mt-5" style="position: relative;">
+                    <h6 class="font-weight-bold d-inline-block mt-4 px-2" style="position: absolute; top: -35px; left: 20px; background: #fff;">Domisili</h6>
+                    <div class="border rounded p-3">
+                        <table class="table table-striped">
+                            <tr>
+                                <td>Provinsi</td>
+                                <td>12356789689</td>
+                            </tr>
+                            <tr>
+                                <td>Kabupaten</td>
+                                <td>12356789689</td>
+                            </tr>
+                            <tr>
+                                <td>Kecamatan</td>
+                                <td>12356789689</td>
+                            </tr>
+                            <tr>
+                                <td>Alamat</td>
+                                <td>12356789689</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-sm btn-danger" data-dismiss="modal">Tutup</button>
             </div>
         </div>
     </div>
