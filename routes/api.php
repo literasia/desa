b773d6562be7876b71bbb46aa0dd4bbea55f5dd0
mugiwara404 <aminugrahaewean@gmail.com>
2021-04-23 13:07:31 +0700

    fix fitur peristiwa

--- routes/api.php
+++ routes/api.php
@@ -121,14 +121,11 @@ Route::namespace('API')
         Route::get('view-group-family/{id}/{village_id}', 'FamilyAPIController@getGroupFamily');
 
         //Birth
-        Route::get('get-birth/{village_id}', 'BirthAPIController@getBirth');
-        Route::post('add-birth/{village_id}', 'BirthAPIController@addBirth');
+        Route::get('get-citizen-birth/{village_id}', 'BirthAPIController@getBirth');
 
         //Death
         Route::get('get-citizen-death/{village_id}', 'DeathAPIController@getDeath');
-        Route::post('add-citizen-death/{village_id}', 'DeathAPIController@addDeath');
         
         //Immigrate
         Route::get('get-citizen-immigrate/{village_id}', 'ImmigrateAPIController@getImmigrate');
-        Route::post('add-citizen-immigrate/{village_id}', 'ImmigrateAPIController@addImmigrate');
     });
