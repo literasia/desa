34a352ecc23cc0ba5c0e0d6c85097db10a4856e0
mugiwara404 <aminugrahaewean@gmail.com>
2021-06-02 09:57:26 +0700

    api lembaga desa

---
+++ app/Http/Controllers/API/CommunityAPIController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Community;
+use App\Models\CommunityType;
+use App\Utils\ApiResponse;
+
+class CommunityAPIController extends Controller
+{
+    public function getCommunity($village_id)
+    {
+        $community = Community::join('community_types', 'communities.community_type_id', 'community_types.id')
+                                ->where('communities.village_id', $village_id)->orderByDesc('community_types.created_at')->get();
+
+        return response()->json(ApiResponse::success($community, 'Success get data'));
+    }
+
+    public function getCommunityFilter($village_id, $types_id)
+    {
+        $community = Community::join('community_types', 'communities.community_type_id', 'community_types.id')
+                                ->where('communities.community_type_id', $types_id)
+                                ->where('communities.village_id', $village_id)->orderByDesc('community_types.created_at')->get();
+
+        return response()->json(ApiResponse::success($community, 'Success get data'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\{Route, Auth};
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 // Superadmin route
